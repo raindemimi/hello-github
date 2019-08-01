1d65bc03224b74d2372611b8c40e1a164272ab49
rain <raindemimi@github.com>
2019-08-01 21:03:51 +0800

    hello-github

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,35 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
-let plugin = requirePlugin("myPlugin")
-let routeInfo = {
-  startLat: 39.90469,    //起点纬度 选填
-  startLng: 116.40717,    //起点经度 选填
-  startName: "我的位置",   // 起点名称 选填
-  endLat: 39.94055,    // 终点纬度必传
-  endLng: 116.43207,  //终点经度 必传
-  endName: "来福士购物中心",  //终点名称 必传
-  mode:'car'  //算路方式 选填
-},
 
 Page({
   data: {
-    routeInfo: routeInfo
+    routeInfo: {
+      startLat: 28.218651, //起点经度 选填
+      startLng: 112.91880, //起点纬度 选填
+      startName: "湖南商学院", // 起点名称 选填
+      endLat: 28.192305, // 终点经度必传
+      endLng: 113.011417, //终点纬度 必传
+      endName: "长沙火车站", //终点名称 必传
+      mode: "car" //算路方式 选填
+    }
   },
-  
-  
+
+  // 生命周期函数--监听页面加载
+  onLoad: function (options) {
+    let _page = this;
+    _page.setData({
+      routeInfo: {
+        startLat: parseFloat(options.latStart), //起点经度 选填
+        startLng: parseFloat(options.lngStart), //起点纬度 选填
+        startName: options.get_address, // 起点名称 选填
+        endLat: parseFloat(options.latEnd), // 终点经度必传
+        endLng: parseFloat(options.lngEnd), //终点纬度 必传
+        endName: options.give_address, //终点名称 必传
+        mode: "car" //算路方式 选填
+
+      }
+    })
+  },
+  // 生命周期函数--监听页面显示
+  onShow: function () {
+    let plugin = requirePlugin("myPlugin");
+  }
 })
